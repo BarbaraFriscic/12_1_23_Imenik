6c0024c92dab352259b965405f581b10cf3f4383
Barbara Friščić <80264267+BarbaraFriscic@users.noreply.github.com>
2023-01-16 08:24:32 +0100

    Add project files.

---
+++ 12_1_23_Imenik/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace _12_1_23_Imenik
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstImenik = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnUnos = new System.Windows.Forms.Button();
+            this.txtIme = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPrezime = new System.Windows.Forms.TextBox();
+            this.txtTrazi = new System.Windows.Forms.TextBox();
+            this.btnTrazi = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstImenik
+            // 
+            this.lstImenik.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lstImenik.FormattingEnabled = true;
+            this.lstImenik.ItemHeight = 15;
+            this.lstImenik.Location = new System.Drawing.Point(23, 29);
+            this.lstImenik.Name = "lstImenik";
+            this.lstImenik.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.lstImenik.Size = new System.Drawing.Size(162, 124);
+            this.lstImenik.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(209, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ime:";
+            // 
+            // btnUnos
+            // 
+            this.btnUnos.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnUnos.Location = new System.Drawing.Point(212, 130);
+            this.btnUnos.Name = "btnUnos";
+            this.btnUnos.Size = new System.Drawing.Size(100, 23);
+            this.btnUnos.TabIndex = 2;
+            this.btnUnos.Text = "Unesi";
+            this.btnUnos.UseVisualStyleBackColor = true;
+            this.btnUnos.Click += new System.EventHandler(this.btnUnos_Click);
+            // 
+            // txtIme
+            // 
+            this.txtIme.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtIme.Location = new System.Drawing.Point(212, 47);
+            this.txtIme.Name = "txtIme";
+            this.txtIme.Size = new System.Drawing.Size(100, 21);
+            this.txtIme.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(209, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Prezime:";
+            // 
+            // txtPrezime
+            // 
+            this.txtPrezime.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtPrezime.Location = new System.Drawing.Point(212, 89);
+            this.txtPrezime.Name = "txtPrezime";
+            this.txtPrezime.Size = new System.Drawing.Size(100, 21);
+            this.txtPrezime.TabIndex = 5;
+            // 
+            // txtTrazi
+            // 
+            this.txtTrazi.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtTrazi.Location = new System.Drawing.Point(65, 198);
+            this.txtTrazi.Name = "txtTrazi";
+            this.txtTrazi.Size = new System.Drawing.Size(120, 21);
+            this.txtTrazi.TabIndex = 6;
+            // 
+            // btnTrazi
+            // 
+            this.btnTrazi.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnTrazi.Location = new System.Drawing.Point(212, 196);
+            this.btnTrazi.Name = "btnTrazi";
+            this.btnTrazi.Size = new System.Drawing.Size(75, 23);
+            this.btnTrazi.TabIndex = 7;
+            this.btnTrazi.Text = "Traži";
+            this.btnTrazi.UseVisualStyleBackColor = true;
+            this.btnTrazi.Click += new System.EventHandler(this.btnTrazi_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(343, 254);
+            this.Controls.Add(this.btnTrazi);
+            this.Controls.Add(this.txtTrazi);
+            this.Controls.Add(this.txtPrezime);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtIme);
+            this.Controls.Add(this.btnUnos);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstImenik);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstImenik;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnUnos;
+        private System.Windows.Forms.TextBox txtIme;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPrezime;
+        private System.Windows.Forms.TextBox txtTrazi;
+        private System.Windows.Forms.Button btnTrazi;
+    }
+}
+
---
+++ 12_1_23_Imenik/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _12_1_23_Imenik
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Osoba x;
+       List<Osoba> Imenik = new List<Osoba>();
+
+        private void btnUnos_Click(object sender, EventArgs e)
+        { 
+            try
+            {
+                if (txtIme.Text != "" && txtPrezime.Text != "")
+                {
+                    x = new Osoba()
+                    {
+                        Ime = txtIme.Text, Prezime = txtPrezime.Text
+                    };
+                    for(int i = 0; i < Imenik.Count(); i++)
+                    {
+                        if (x.Ime == Imenik[i].Ime && x.Prezime == Imenik[i].Prezime)
+                            throw new ArgumentException();
+                    }    
+                    Imenik.Add(x);
+                    lstImenik.Items.Add(x);
+
+                    txtIme.Clear();
+                    txtPrezime.Clear();
+                    txtIme.Focus();
+                }
+                else
+                {
+                    MessageBox.Show("Sva polja su obvezna!");
+                    if (txtIme.Text == "")
+                        txtIme.Focus();
+                    else
+                        txtPrezime.Focus();
+                }
+                    
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Ispravite svoj unos!");
+                txtIme.Focus();
+            }
+           catch (ArgumentException)
+            {
+                MessageBox.Show("Osoba već postoji u imeniku!");
+                txtIme.Clear();
+                txtPrezime.Clear();
+                txtIme.Focus();
+            }
+        }
+
+        private void btnTrazi_Click(object sender, EventArgs e)
+        {   lstImenik.SelectedItems.Clear();
+            bool pronadjen = false;
+            for(int i = 0; i < lstImenik.Items.Count; i++)
+            {
+                Osoba temp = (Osoba)lstImenik.Items[i];
+                if(temp.Ime == txtTrazi.Text || temp.Prezime == txtTrazi.Text || temp.ToString() == txtTrazi.Text)
+                {
+                    pronadjen = true;
+                    lstImenik.SelectedItems.Add(temp);
+                }
+            }
+            if(pronadjen == false)
+            {
+                MessageBox.Show("Tražena osoba nije pronađena.");
+                txtTrazi.Focus();
+            }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtIme.Focus();
+        }
+    }
+}
